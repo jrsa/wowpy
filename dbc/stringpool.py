682af56a879368d789f0af56403394c4db5c1bb5
James Anderson <jrsa@jrsa.co>
2016-06-17 11:06:10 -0700

    basic class for interning strings into one block of strings, separated by nulls. one method to "look up" a string, which returns an offset for that string into the block.

---
+++ dbc/stringpool.py
@@ -0,0 +1,29 @@
+from zlib import adler32
+
+
+class StringPool():
+
+    """
+    stores strings from the records in a contiguous block,
+    separated by null terminators. provides the offset.
+    if an already extant string is pushed in, the offset to
+    the existing version is given and a new copy is NOT stored.
+    """
+
+    def __init__(self):
+        self.block = bytearray()
+        self.table = dict()
+
+    def lookup(self, string):
+
+        hshstr = adler32(string)
+
+        try:
+            offset = self.table[hshstr]
+        except KeyError, e:
+            offset = len(self.block)
+            self.block.extend(string)
+            self.block.append('\x00')
+            self.table[hshstr] = offset
+
+        return offset
