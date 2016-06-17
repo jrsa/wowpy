4169be5eed83df04e9144494d7078f6adb64ac26
James Anderson <jrsa@jrsa.co>
2016-06-17 11:57:45 -0700

    not gonna have files read in constructors

--- adt/__init__.py
+++ adt/__init__.py
@@ -1,6 +1,11 @@
 import file_format
+import chunks
 
 
 class AdtFile(object):
-  def __init__(self, data):
-    self.chunks = []
+    def __init__(self):
+        self.chunks = []
+        self.doodad_refs = []
+        self.mapobject_refs = []
+
+    
