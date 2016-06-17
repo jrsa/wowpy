1db9495cd3b3d775c1b629352048a60790f85ce9
James Anderson <jrsa@jrsa.co>
2016-06-17 11:33:02 -0700

    format pass of chunks.py and added makechunk()

--- chunks.py
+++ chunks.py
@@ -3,11 +3,21 @@ from struct import Struct
 
 chunk = Struct('<4si')
 
+
 def chunks(data):
-  counter, filesize = 0, len(data)
-  while counter < filesize:
-    magic, size = chunk.unpack_from(data, counter)
-    counter += chunk.size
-    contents = data[counter:counter+size]
-    yield magic, size, contents
-    counter += size
+    """
+    generator giving all off the IFF chunks in a buffer
+    does not handle bad input :(
+    """
+    counter, filesize = 0, len(data)
+    while counter < filesize:
+        magic, size = chunk.unpack_from(data, counter)
+        counter += chunk.size
+        contents = data[counter:counter+size]
+        yield magic, size, contents
+        counter += size
+
+def makechunk(cc, data):
+    assert type(cc) == str
+    assert type(data) == str
+    return cc + str( len(data) ) + data
