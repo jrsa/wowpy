5723c2e6ac8407075cc62b07a7c5b8f71f2e1c17
James Anderson <jrsa@jrsa.co>
2017-07-11 18:05:10 -0700

    first effort towards adt file loading

--- adt/__init__.py
+++ adt/__init__.py
@@ -3,19 +3,26 @@ from .. import chunks
 import struct
 
 
-class AdtFile(object):
-    def __init__(self):
-        self.chunks = []
-        self.doodad_refs = []
-        self.mapobject_refs = []
-        self.chunk_header = struct.Struct('')
+raise ImportError("borken")
 
-    
+
+class ChunkType(Enum):
+    string = 1
+    stringarray = 2
+    struct = 3
+    structarray = 4
+
+
+formats = {
+    b"TVCM": "f"*145,
+}
+
+
+class AdtFile(object):
+
     def load(self, data):
         for cc, size, contents in chunks.chunks(data):
-            print(cc)
-            if cc == 'KNCM':
-                print(struct.unpack('I' * 32, contents[:128]))
+            if cc == b'KNCM':
+                hdr = self.chunk_header.unpack(contents[:128])
                 for cc, size, contents in chunks.chunks(contents[128:]):
-                   print(cc, size)
-    
+                    
--- chunks.py
+++ chunks.py
@@ -3,6 +3,12 @@ from struct import Struct
 
 chunk = Struct('<4si')
 
+"""
+chunks which contain unidentified bytes (headerless) and
+fuck up the loading
+"""
+badchunks = {b'RNCM': 13}
+
 
 def chunks(data):
     """
@@ -12,12 +18,15 @@ def chunks(data):
     counter, filesize = 0, len(data)
     while counter < filesize:
         magic, size = chunk.unpack_from(data, counter)
+        if magic in badchunks:
+            counter += badchunks[magic] # seek past extra bytes
         counter += chunk.size
         contents = data[counter:counter+size]
         yield magic, size, contents
         counter += size
 
+
 def makechunk(cc, data):
     assert type(cc) == str
     assert type(data) == str
-    return cc + str( len(data) ) + data
+    return cc + str(len(data)) + data
