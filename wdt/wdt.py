d91aa1607f04292180d52a2a5efd3523a678a565
James Anderson <jrsa@jrsa.co>
2018-08-23 10:36:05 -0700

    full rewrite using new chunk parsing

--- wdt/wdt.py
+++ wdt/wdt.py
@@ -3,51 +3,35 @@ wdt file parsing
 """
 
 import struct
-from ..chunks import chunks, makechunk
+from ..chunks import parse
+
+from .format import chunks as wdt_chunk_formats
+
+from itertools import product
 
 
 MAP_SIZE = 64
 
 
 class Wdt(object):
-  def __init__(self, version=18):
-    self.version = version
-    self.extant_tiles = []
-    self.flags = 0
-    self.main_rec = struct.Struct('II')
-    self.objdef_rec = struct.Struct('IIffffffffffffHHHH')
-    self.object_filename = None
-
-  def parse_modf(self, data):
-    self.obj = self.objdef_rec.unpack(data)
-
   def read(self, data):
     """takes file as bytes"""
-    if data[:4] != b'REVM' or data[0x34:0x38] != b'NIAM':
-      raise RuntimeError('not a wdt file')
+    parsed_chunks = parse(data, wdt_chunk_formats)
 
-    for id, size, data in chunks(data):
-      if id == b'REVM':
-        self.version = struct.unpack('I', data)[0]
+    self.version = parsed_chunks['MVER'][0].version
+    self.flags = parsed_chunks['MPHD'][0].flags
+    self.object_filename = parsed_chunks['MWMO'][0]
 
-      if id == b'NIAM':
-        if len(data) != 32768:
-          raise RuntimeError('invalid MAIN sect')
+    try:
+      self.obj = parsed_chunks['MODF'][0]
+    except KeyError:
+      pass
 
-        for i in range(64):
-          for j in range(64):
-              idx = (j * 64 + i) * self.main_rec.size
-              flag = self.main_rec.unpack_from(data, idx)[0]
-              if flag & 1 == 1:
-                self.extant_tiles.append((i, j))
+    self.extant_tiles = []
 
-      elif id == b'DHPM':
-        self.flags = struct.unpack('iiiiiiii', data)[0]
-
-      if id == b'OMWM':
-        self.object_filename = data[:-1]
-      elif id == b'FDOM':
-        self.parse_modf(data)
+    for x, y in product(range(MAP_SIZE), range(MAP_SIZE)):
+      if parsed_chunks['MAIN'][x + (y * MAP_SIZE)].flags & 1:
+        self.extant_tiles.append((x, y),)
 
   def write(self):
     """returns a string to be written to file"""
